aeb4d8b566991db9c04b550fd69667160d16f587
John Tang <2238192070@qq.com>
2020-10-30 16:47:05 +0800

    项目初始化
    0v0

---
+++ src/main/java/com/sicnu/oasystem/OasystemApplication.java
@@ -0,0 +1,14 @@
+package com.sicnu.oasystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class OasystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OasystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sicnu/oasystem/OasystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sicnu.oasystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OasystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
