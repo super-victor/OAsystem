a4c61fba3a968c594a58d38e9927ea55dca6a24b
waynejwei <513512942@qq.com>
2020-11-03 22:31:40 +0800

    添加sql表

--- table.sql
+++ table.sql
@@ -6,4 +6,40 @@ create table Department(
     phone char(11),
     createTime TIMESTAMP,
     updateTime TIMESTAMP
-)
+);
+/*创建职工表*/
+create table Employee(
+                         employeeId int primary key auto_increment,
+                         name varchar(20),
+                         password varchar(20),
+                         phone char(11),
+                         email varchar(20),
+                         birth timestamp,
+                         idCard char(18),
+                         sex char(2),
+                         employmentDate timestamp,
+                         departmentId int,
+                         position varchar(10),
+                         homeLocation varchar(20),
+                         createTime timestamp,
+                         updateTime timestamp
+);
+/*创建日程表*/
+create table Schedule(
+                         scheduleId int primary key auto_increment,
+                         time timestamp,
+                         location varchar(50),
+                         work text,
+                         comment text,
+                         type int,
+                         createTime timestamp,
+                         updateTime timestamp
+);
+/*创建职工日程对应表*/
+create table EmployeeSchedule(
+                                 employeeScheduleId int primary key auto_increment,
+                                 scheduleId int,
+                                 employeeId int,
+                                 createTime timestamp,
+                                 updateTime timestamp
+);
