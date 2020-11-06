760e500735cde9d92e878cfc673e5b5612edb926
waynejwei <513512942@qq.com>
2020-11-06 20:25:40 +0800

    完成名片夹mapper

--- src/main/java/com/sicnu/oasystem/mapper/CardHolderClassfyMapper.java
+++ src/main/java/com/sicnu/oasystem/mapper/CardHolderClassfyMapper.java
@@ -1,5 +1,6 @@
 package com.sicnu.oasystem.mapper;
 
+import com.sicnu.oasystem.pojo.CardHolder;
 import com.sicnu.oasystem.pojo.CardHolderClassfy;
 import org.apache.ibatis.annotations.Mapper;
 import org.springframework.stereotype.Component;
@@ -60,4 +61,67 @@ public interface CardHolderClassfyMapper {
      */
     int insertCardHolderClassfyByCardHolderClassfyId(int employeeId, String name);
 
+    /**
+     * @MethodName findCardHolderByCardHolderClassfyId
+     * @param CardHolderClassfyId
+     * @Description 查找名片夹by名片夹分类id
+     * @Author Waynejwei
+     * @Return java.util.List<com.sicnu.oasystem.pojo.CardHolder>
+     * @LastChangeDate 2020/11/6
+     */
+    List<CardHolder> findCardHolderByCardHolderClassfyId(int CardHolderClassfyId);
+
+    /**
+     * @MethodName findCardHolderByEmployeeId
+     * @param EmployeeId
+     * @Description 查找名片夹by职工id
+     * @Author Waynejwei
+     * @Return java.util.List<com.sicnu.oasystem.pojo.CardHolder>
+     * @LastChangeDate 2020/11/6
+     */
+    List<CardHolder> findCardHolderByEmployeeId(int EmployeeId);
+
+    /**
+     * @MethodName findCardHolderByOwnedId
+     * @param ownerId
+     * @param ownedId
+     * @Description 查找某一职工拥有的另一个职工的名片夹
+     * @Author Waynejwei
+     * @Return com.sicnu.oasystem.pojo.CardHolder
+     * @LastChangeDate 2020/11/6
+     */
+    CardHolder findCardHolderByOwnedId(int ownerId, int ownedId);
+
+    /**
+     * @MethodName deleteCardHolderByCardHolderId
+     * @param CardHolderId
+     * @Description 删除名片夹by他人的职工id
+     * @Author Waynejwei
+     * @Return int
+     * @LastChangeDate 2020/11/6
+     */
+    int deleteCardHolderByCardHolderId(int CardHolderId);
+
+    /**
+     * @MethodName insertCardHolderByOwnedId
+     * @param ownerId
+     * @param ownedId
+     * @param cardHolderClassfyId
+     * @Description 增加名片夹
+     * @Author Waynejwei
+     * @Return int
+     * @LastChangeDate 2020/11/6
+     */
+    int insertCardHolderByOwnedId(int ownerId, int ownedId, int cardHolderClassfyId);
+
+    /**
+     * @MethodName updateCardHolderByCardHolderId
+     * @param CardHolderId
+     * @param cardHolderClassfyId
+     * @Description 修改名片夹分类by名片夹id
+     * @Author Waynejwei
+     * @Return int
+     * @LastChangeDate 2020/11/6
+     */
+    int updateCardHolderByCardHolderId(int CardHolderId, int cardHolderClassfyId);
 }
