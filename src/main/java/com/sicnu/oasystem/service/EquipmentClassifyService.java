5f2f488382fe05245f2580a7b5efff45fc2cace1
super-victor <1358844623@qq.com>
2020-11-17 10:46:24 +0800

    添加EuqipmentClassifyService和EquipmentService

--- src/main/java/com/sicnu/oasystem/service/EquipmentClassifyService.java
+++ src/main/java/com/sicnu/oasystem/service/EquipmentClassifyService.java
@@ -1,5 +1,13 @@
 package com.sicnu.oasystem.service;
 
+import com.sicnu.oasystem.json.BackFrontMessage;
+import com.sicnu.oasystem.mapper.EquipmentClassifyMapper;
+import com.sicnu.oasystem.mapper.EquipmentMapper;
+import com.sicnu.oasystem.pojo.EquipmentClassify;
+
+import javax.annotation.Resource;
+import java.util.List;
+
 /**
  * @InterfaceName EquipmentClassifyService
  * @Description
@@ -9,4 +17,46 @@ package com.sicnu.oasystem.service;
  */
 
 public interface EquipmentClassifyService {
+
+    /**
+     * @MethodName addEquipmentClassify
+     * @param classifyName
+     * @Description 添加设备分类
+     * @Author pan
+     * @Return com.sicnu.oasystem.json.BackFrontMessage
+     * @LastChangeDate 2020/11/16
+     */
+    BackFrontMessage addEquipmentClassify(String classifyName);
+
+    /**
+     * @MethodName updateEquipmentClassify
+     * @param classifyId
+     * @param newClassifyName
+     * @Description 修改设备分类
+     * @Author pan
+     * @Return com.sicnu.oasystem.json.BackFrontMessage
+     * @LastChangeDate 2020/11/16
+     */
+    BackFrontMessage updateEquipmentClassify(Integer classifyId,String newClassifyName);
+
+    /**
+     * @MethodName deleteEquipmentClassify
+     * @param ClassifyId
+     * @Description 删除设备分类
+     * @Author pan
+     * @Return com.sicnu.oasystem.json.BackFrontMessage
+     * @LastChangeDate 2020/11/16
+     */
+    BackFrontMessage deleteEquipmentClassify(Integer ClassifyId);
+
+    /**
+     * @MethodName getAllEquipmentClassify
+     * @param
+     * @Description 获取所有设备分类
+     * @Author pan
+     * @Return com.sicnu.oasystem.json.BackFrontMessage
+     * @LastChangeDate 2020/11/16
+     */
+    BackFrontMessage getAllEquipmentClassify();
+
 }
---
+++ src/main/java/com/sicnu/oasystem/service/EquipmentService.java
@@ -0,0 +1,69 @@
+package com.sicnu.oasystem.service;
+
+import com.sicnu.oasystem.json.BackFrontMessage;
+import com.sicnu.oasystem.pojo.Equipment;
+
+import java.util.List;
+
+/**
+ * @InterfaceName EquipmentService
+ * @Description
+ * @Author pan
+ * @LastChangeDate 2020/11/16 23:36
+ * @Version v1.0
+ */
+
+public interface EquipmentService {
+
+    /**
+     * @MethodName addEquipment
+     * @param equipmentId
+     * @param equipmentclassifyId
+     * @param meetingroomId
+     * @param name
+     * @param ismaintain
+     * @param num
+     * @Description 添加设备
+     * @Author pan
+     * @Return com.sicnu.oasystem.json.BackFrontMessage
+     * @LastChangeDate 2020/11/16
+     */
+    BackFrontMessage addEquipment(Integer equipmentId, Integer equipmentclassifyId,
+                                  Integer meetingroomId, String name, Integer ismaintain, int num);
+
+    /**
+     * @MethodName updateEquipment
+     * @param equipmentId
+     * @param equipmentclassifyId
+     * @param newmeetingroomId
+     * @param newname
+     * @param newismaintain
+     * @param newnum
+     * @Description 更新设备
+     * @Author pan
+     * @Return com.sicnu.oasystem.json.BackFrontMessage
+     * @LastChangeDate 2020/11/16
+     */
+    BackFrontMessage updateEquipment(Integer equipmentId,Integer equipmentclassifyId,
+                            Integer newmeetingroomId,String newname,Integer newismaintain,int newnum);
+
+    /**
+     * @MethodName deleteEuipment
+     * @param equipmentId
+     * @Description 删除设备
+     * @Author pan
+     * @Return com.sicnu.oasystem.json.BackFrontMessage
+     * @LastChangeDate 2020/11/16
+     */
+    BackFrontMessage deleteEuipment(Integer equipmentId);
+
+    /**
+     * @MethodName getallEquipment
+     * @param
+     * @Description 获取所有设备
+     * @Author pan
+     * @Return com.sicnu.oasystem.json.BackFrontMessage
+     * @LastChangeDate 2020/11/16
+     */
+    BackFrontMessage getallEquipment();
+}
