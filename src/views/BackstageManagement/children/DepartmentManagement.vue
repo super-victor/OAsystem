ea755fd25711db53c041403db001d73ec9408758
lkx <1336354338@qq.com>
2020-11-20 21:37:49 +0800

    为rem设置最小值(屏幕小于1000px后不再变化)

---
+++ src/views/BackstageManagement/children/DepartmentManagement.vue
@@ -0,0 +1,36 @@
+!<!-- DepartmentManagement -->
+<template>
+  <div class='DepartmentManagement'>
+
+  </div>
+</template>
+
+<script>
+  export default {
+    components: {},
+    data() {
+      return {
+        
+      };
+    },
+    computed: {
+    },
+    watch: {},
+    methods: {
+    },
+    created() {
+      
+    },
+    mounted() {
+      this.$emit('childrenBread',['部门管理']);
+    }
+  }
+</script>
+<style lang='less' scoped>
+  .DepartmentManagement{
+    height: 500px;
+    width: 100%;
+    background-color: salmon;
+    font-size: 30px;
+  }
+</style>
---
+++ src/views/BackstageManagement/children/RoleManagement.vue
@@ -0,0 +1,36 @@
+!<!-- RoleManagement -->
+<template>
+  <div class='RoleManagement'>
+
+  </div>
+</template>
+
+<script>
+  export default {
+    components: {},
+    data() {
+      return {
+        
+      };
+    },
+    computed: {
+    },
+    watch: {},
+    methods: {
+    },
+    created() {
+      
+    },
+    mounted() {
+      this.$emit('childrenBread',['角色管理']);
+    }
+  }
+</script>
+<style lang='less' scoped>
+  .RoleManagement{
+    height: 500px;
+    width: 100%;
+    background-color: salmon;
+    font-size: 30px;
+  }
+</style>
