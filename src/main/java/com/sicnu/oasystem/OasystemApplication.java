83f7a946ac8dd085f6f7640672ee7845be04ff33
waynejwei <513512942@qq.com>
2020-12-01 19:34:03 +0800

    设置application的transaction

--- src/main/java/com/sicnu/oasystem/OasystemApplication.java
+++ src/main/java/com/sicnu/oasystem/OasystemApplication.java
@@ -3,10 +3,12 @@ package com.sicnu.oasystem;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 
 // 协同办公系统
 @SpringBootApplication
+@EnableTransactionManagement
 public class OasystemApplication {
 
     public static void main(String[] args) {
